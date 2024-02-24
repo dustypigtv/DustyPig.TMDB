23004a6ae09ede9ba5416d60068774e8754621c3
Jason <dev@dustypig.tv>
2024-02-24 13:05:31 -0700

    Initial Tests project

---
+++ TestsProject/ClientFactory.cs
@@ -0,0 +1,26 @@
+﻿using DustyPig.TMDB;
+
+namespace TestsProject;
+
+static class ClientFactory
+{
+    private static readonly object _locker = new();
+    private static Client _client = null;
+
+    public static Client GetClient()
+    {
+        lock (_locker)
+        {
+            if (_client == null)
+                _client = new(Client.AuthTypes.APIKey, Environment.GetEnvironmentVariable("TMDB_API_KEY"))
+                {
+                    RetryCount = 10,
+                    RetryDelay = 250,
+                    AutoThrowIfError = true,
+                    IncludeRawContentInResponse = true
+                };
+        }
+        return _client;
+    }
+   
+}
---
+++ TestsProject/TestCertifications.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestsProject;
+
+[TestClass]
+public class TestCertifications
+{
+    [TestMethod]
+    [DoNotParallelize]
+    public async Task GetMovieCertifications()
+    {
+        await ClientFactory.GetClient().Endpoints.Certifications.GetMovieCertificationsAsync();
+    }
+
+    [TestMethod]
+    [DoNotParallelize]
+    public async Task GetTvCertifications()
+    {
+        await ClientFactory.GetClient().Endpoints.Certifications.GetTvCertificationsAsync();
+    }
+
+}
