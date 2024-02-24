42dd504819a2b5dbd1ee003510ff6de95d545f10
Jason <dev@dustypig.tv>
2024-02-24 12:14:09 -0700

    Inherit from common

--- DustyPig.TMDB/Models/Authentication/ValidateKey.cs
+++ DustyPig.TMDB/Models/Authentication/ValidateKey.cs
@@ -1,15 +1,10 @@
+using DustyPig.TMDB.Models.Common;
 using System.Text.Json.Serialization;
 
 namespace DustyPig.TMDB.Models.Authentication;
 
-public class ValidateKey
+public class ValidateKey : CommonStatus
 {
-    [JsonPropertyName("status_code")]
-    public int StatusCode { get; set; }
-
-    [JsonPropertyName("status_message")]
-    public string StatusMessage { get; set; }
-
     [JsonPropertyName("success")]
     public bool Success { get; set; }
 }
