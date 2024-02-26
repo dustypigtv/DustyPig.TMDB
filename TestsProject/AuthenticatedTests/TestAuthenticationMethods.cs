2bdd0ca7a3ac9cff556fb515c9a688dc65d67c59
Jason <dev@dustypig.tv>
2024-02-25 21:32:00 -0700

    Comment out unused tests

--- TestsProject/AuthenticatedTests/TestAuthenticationMethods.cs
+++ TestsProject/AuthenticatedTests/TestAuthenticationMethods.cs
@@ -11,28 +11,28 @@ public class TestAuthenticationMethods
     //Test this maually
     //[TestMethod]
     //[DoNotParallelize]
-    public async Task CreateSession()
-    {
-        var client = ClientFactory.GetClient();
+    //public async Task CreateSession()
+    //{
+    //    var client = ClientFactory.GetClient();
 
 
-        //Step 1: Get a request token
-        var createRequestTokenResponse = await client.Endpoints.Authentication.GetCreateRequestTokenAsync();
+    //    //Step 1: Get a request token
+    //    var createRequestTokenResponse = await client.Endpoints.Authentication.GetCreateRequestTokenAsync();
 
-        //Step 2: Visit this link and approve the session
-        var approveUrl = "https://www.themoviedb.org/authenticate/" + createRequestTokenResponse.Data.RequestToken;
-        var breakpointHere = true;
+    //    //Step 2: Visit this link and approve the session
+    //    var approveUrl = "https://www.themoviedb.org/authenticate/" + createRequestTokenResponse.Data.RequestToken;
+    //    var breakpointHere = true;
 
-        //Step 3: Create a sessionId
-        CreateSessionRequest createSessionRequest = new() { RequestToken = createRequestTokenResponse.Data.RequestToken };
-        var createSessionResponse = await client.Endpoints.Authentication.CreateSessionAsync(createSessionRequest);
-        string sessionId = createSessionResponse.Data.SessionId;
+    //    //Step 3: Create a sessionId
+    //    CreateSessionRequest createSessionRequest = new() { RequestToken = createRequestTokenResponse.Data.RequestToken };
+    //    var createSessionResponse = await client.Endpoints.Authentication.CreateSessionAsync(createSessionRequest);
+    //    string sessionId = createSessionResponse.Data.SessionId;
 
 
-        //Step 4: Delete the sessionId
-        var deleteSessionRequest = new DeleteSessionRequest { SessionId = sessionId };
-        await client.Endpoints.Authentication.DeleteSessionAsync(deleteSessionRequest);
-    }
+    //    //Step 4: Delete the sessionId
+    //    var deleteSessionRequest = new DeleteSessionRequest { SessionId = sessionId };
+    //    await client.Endpoints.Authentication.DeleteSessionAsync(deleteSessionRequest);
+    //}
 
 
 
@@ -51,50 +51,50 @@ public class TestAuthenticationMethods
     //Test this maually
     //[TestMethod]
     //[DoNotParallelize]
-    public async Task CreateSessionFromV4Token()
-    {
-        CreateSessionFromV4TokenRequest postData = new()
-        {
-            AccessToken = ""
-        };
+    //public async Task CreateSessionFromV4Token()
+    //{
+    //    CreateSessionFromV4TokenRequest postData = new()
+    //    {
+    //        AccessToken = ""
+    //    };
 
-        await ClientFactory.GetClient().Endpoints.Authentication.CreateSessionFromV4TokenAsync(postData);
-    }
+    //    await ClientFactory.GetClient().Endpoints.Authentication.CreateSessionFromV4TokenAsync(postData);
+    //}
 
 
 
     //Test this maually
     //[TestMethod]
     //[DoNotParallelize]
-    public async Task CreateSessionWithLogin()
-    {
-        var client = ClientFactory.GetClient();
+    //public async Task CreateSessionWithLogin()
+    //{
+    //    var client = ClientFactory.GetClient();
 
 
-        //Step 1: Get a request token
-        var createRequestTokenResponse = await client.Endpoints.Authentication.GetCreateRequestTokenAsync();
+    //    //Step 1: Get a request token
+    //    var createRequestTokenResponse = await client.Endpoints.Authentication.GetCreateRequestTokenAsync();
 
 
-        //Step 2: Login
-        CreateSessionWithLoginRequest postData = new()
-        {
-            Username = Environment.GetEnvironmentVariable("TMDB_API_USERNAME"),
-            Password = Environment.GetEnvironmentVariable("TMDB_API_PASSWORD"),
-            RequestToken = createRequestTokenResponse.Data.RequestToken
-        };
-        var createSessionFromLoginResponse = await client.Endpoints.Authentication.CreateSessionWithLoginAsync(postData);
+    //    //Step 2: Login
+    //    CreateSessionWithLoginRequest postData = new()
+    //    {
+    //        Username = Environment.GetEnvironmentVariable("TMDB_API_USERNAME"),
+    //        Password = Environment.GetEnvironmentVariable("TMDB_API_PASSWORD"),
+    //        RequestToken = createRequestTokenResponse.Data.RequestToken
+    //    };
+    //    var createSessionFromLoginResponse = await client.Endpoints.Authentication.CreateSessionWithLoginAsync(postData);
 
 
-        //Step 3: Create a sessionId
-        CreateSessionRequest createSessionRequest = new() { RequestToken = createSessionFromLoginResponse.Data.RequestToken };
-        var createSessionResponse = await client.Endpoints.Authentication.CreateSessionAsync(createSessionRequest);
-        string sessionId = createSessionResponse.Data.SessionId;
+    //    //Step 3: Create a sessionId
+    //    CreateSessionRequest createSessionRequest = new() { RequestToken = createSessionFromLoginResponse.Data.RequestToken };
+    //    var createSessionResponse = await client.Endpoints.Authentication.CreateSessionAsync(createSessionRequest);
+    //    string sessionId = createSessionResponse.Data.SessionId;
 
 
-        //Step 4: Delete the sessionId
-        var deleteSessionRequest = new DeleteSessionRequest { SessionId = sessionId };
-        await client.Endpoints.Authentication.DeleteSessionAsync(deleteSessionRequest);
-    }
+    //    //Step 4: Delete the sessionId
+    //    var deleteSessionRequest = new DeleteSessionRequest { SessionId = sessionId };
+    //    await client.Endpoints.Authentication.DeleteSessionAsync(deleteSessionRequest);
+    //}
 
 
 
