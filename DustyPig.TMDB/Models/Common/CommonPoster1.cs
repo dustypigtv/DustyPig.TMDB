4b0cfa712a7b837bfd9afa06747fe8741d5fac58
Jason Davis <jasondavis303@gmail.com>
2026-08-31 10:17:57 -0600

    Change all string? to string

--- DustyPig.TMDB/Models/Common/CommonPoster1.cs
+++ DustyPig.TMDB/Models/Common/CommonPoster1.cs
@@ -14,14 +14,14 @@ public class CommonPoster1 : ModelBase
     /// ISO-639-1 2 letter language code. E.g. &quot;en&quot;
     /// </summary>
     [JsonPropertyName("iso_639_1")]
-    public string? ISO_639_1 { get; set; }
+    public string ISO_639_1 { get; set; }
 
     /// <summary>
     /// Links to <see cref="ISO_639_1"/>. 
     /// This property is not serialized.
     /// </summary>
     [JsonIgnore]
-    public string? LanguageCode
+    public string LanguageCode
     {
         get => ISO_639_1;
         set => ISO_639_1 = value;
